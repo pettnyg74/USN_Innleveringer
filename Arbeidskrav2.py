1a7a28982cb042fa60d4fa2d09342c7299c2a673
pettnyg74 <pettnyg@gmail.com>
2025-01-17 16:51:18 +0100

    Update Arbeidskrav2.py

--- Arbeidskrav2.py
+++ Arbeidskrav2.py
@@ -6,31 +6,67 @@ Created on Fri Jan 17 15:29:32 2025
 @mail pettnyg@gmail.com
 
 """
-# #Oppgave 1
-# #Regn ut alder basert på input
-# from datetime import date
+#Oppgave 1
+#Regn ut alder basert på input
+from datetime import date
 
-# #Ber om fødselsår
-# alder = int(input('Hvilket år er du født?'))
+#Ber om fødselsår
+alder = int(input('Hvilket år er du født?'))
 
-# #Finner årstallet i dag
-# dato = date.today()
-# aarstall = int(dato.strftime("%Y"))
-# #Regner ut alder uavhegig av dato
-# alder = aarstall-alder
+#Finner årstallet i dag
+dato = date.today()
+aarstall = int(dato.strftime("%Y"))
+#Regner ut alder uavhegig av dato
+alder = aarstall-alder
 
-# print("Du er eller blir",  alder  ,"år nå i", aarstall)
+print("Du er eller blir",  alder  ,"år nå i", aarstall)
 
 
 #Oppgave 2
 import math
 # Regn ut antall pizzaer til oppgitt antall elever. Beregn 1/4 pizza pr elev
 #Antall elever
-antallElever = int(input("Skriv inn antall elever"))
+antallElever = int(input("Skriv inn antall elever:"))
 
-#Antall hele pizzaer
+#Regner ut antall hele pizzaer
 pizzaer = int(math.ceil(antallElever/4))
 
 print("Det må handles inn",pizzaer,"til festen.")
 
+#Oppgave 3
+#Regn om fra grader til radianer
+import numpy as np
+#Oppgi antall grader
+v_grad = float(input("Skriv inn gradtallet:"))
+
+#Omregning til Radianer
+v_rad = v_grad*np.pi/180
+
+print(v_grad,"grader tilsvarer",v_rad,"radianer.")
+
+#Oppgave 4
+#a)
+#Opprett dictionary
+data = {"Norge":["Oslo", 0.634], "England": ["London", 8.982], "Frankrike": ["Paris", 2.161], "Italia": ["Roma", 2.873]}
+
+#b)
+#Gi ut info om land som tastes inn
+#Ber om landet
+finnLand = str(input("Skriv inn et land:"))
+
+def hentLand(land):
+    if data.get(finnLand) is not None:
+        landInfo = data.get(finnLand)
+        hovedstad = landInfo[0]
+        innbyggere = landInfo[1]
+        out = hovedstad,"er hovedstaden i", finnLand ,"og det er",innbyggere,"mill. innbyggere i", hovedstad
+    
+    
+    else:
+        out = "Kunne ikke finne landet du spurte etter.Har du husket stor forbokstav?"
+    
+    return str(out)
+    
+
+print(hentLand(finnLand))
 
